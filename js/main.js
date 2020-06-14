f088522eb7a7421bd65772dbef112f78b5f50a44
Hristofor Alekseev <hristofor2003@mail.ru>
2020-06-15 00:40:46 +0500

    написал всё,не использовал циклы

--- js/main.js
+++ js/main.js
@@ -1,100 +1,33 @@
 "use strict";
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-let money;
+alert("Загадывание случайного числа от 1 до 100");
 
-let start = function () {
-  let sum1 = 0;
-  do {
-    money = +prompt("Ваш месячный доход:");
-  } while (!isNumber(money));
-  {
-    sum1 += money;
-  }
-  // console.log(money);
-  return money;
-};
+let num = Number;
 
-start();
+function gameBot() {
+  let answer = +prompt("Угадай число от 1 до 100: ");
+  let str = String;
+  console.log(answer);
 
-let income = "Автомойка",
-  addExpenses = prompt("Перечислите возможные расходы  через запятую:"),
-  deposit = confirm("Есть ли у вас депозит в банке?", true),
-  mission = 100000,
-  period = 12,
-  month = 30;
-
-//тип данных
-let showTypeOf = function (data) {
-  console.log(data);
-};
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-//Разбитие строки на массив
-console.log(addExpenses.split());
-//Переменная с присваиванием бюджета
-
-//Урок 3
-
-//Вызов 4-х переменных
-
-let expenses1, expenses2;
-
-let getExpensesMonth = function () {
-  let sum = 0;
-  let amount = 0;
-
-  for (let i = 0; i < 2; i++) {
-    if (i === 0) {
-      expenses1 = prompt("Введите обязательную статью расходов");
-    } else if (i === 1) {
-      expenses2 = prompt("Введите обязательную статью расходов");
+  function getNumber() {
+    if (answer === num) {
+      answer = +prompt("Угадай число от 1 до 100: ");
+    } else if (answer === false) {
+      alert('Игра закончена!');
+    } else if (answer > 100) {
+      confirm("Загаданное число больше!Введите еще раз");
+      prompt("Угадай число от 1 до 100: ");
+    } else if (answer < 100) {
+      confirm("Загаданное число меньше!Введите еще раз");
+      prompt("Угадай число от 1 до 100: ");
+    } else if (answer === str) {
+      confirm("Вы ввели не число"); +
+      prompt("Введите число");
+    } else {
+      confirm("Вы угадали число)");
     }
-
-    do {
-      amount = +prompt("Во сколько это обойдется?");
-    } while (!isNumber(amount));
-    sum += amount;
-    console.log(sum);
   }
-  return sum;
-};
-let expensesAmount = getExpensesMonth();
-
-function getAccumulatedMonth() {
-  return money - expensesAmount;
+  getNumber();
+  console.log(answer);
+  return answer;
 }
-getAccumulatedMonth();
-console.log(getAccumulatedMonth());
-
-let accumulatedMonth = getAccumulatedMonth();
-
-function getTargetMonth() {
-  let getTarget = mission / accumulatedMonth;
-  if (getTarget < 0) {
-    console.log("Цель не будет достигнута");
-  } else {
-    console.log("Цель  будет достигнута за: ", getTarget);
-  }
-  return getTarget;
-}
-getTargetMonth();
-
-let budgetDay = accumulatedMonth / month;
-console.log("Бюджет на день:", Math.floor(budgetDay, -1));
-
-let getStatusIncome = function () {
-  if (budgetDay >= 1200) {
-    return "У вас высокий уровень дохода";
-  } else if (budgetDay > 600 && budgetDay < 1200) {
-    return "У вас средний уровень дохода";
-  } else if (budgetDay < 600) {
-    return "К сожалению у вас уровень дохода ниже среднего";
-  } else if (budgetDay < 0) {
-    return "Вам срочно пора на работу!";
-  }
-};
-console.log(getStatusIncome());
+gameBot();
---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let isNum = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const gameStart = function () {
+  const randomNum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  let count = 10;
+  alert("Угадайте число от 1 до 100!");
+  return function gamePlay() {
+    let num = prompt("Угадайте число");
+    if (isNum(num)) {
+      let useNumber = +num;
+      count--;
+      if (count >= 0) {
+        if (useNumber < randomNum) {
+          confirm("Загаданное число больше,осталось попыток:" + count);
+          return gamePlay();
+        } else if (useNumber > randomNum) {
+          confirm("Загаданное число меньше,осталось попыток:" + count);
+          return gamePlay();
+        } else {
+          if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+            game()();
+          } else {
+            alert("Вы завершили игру");
+          }
+        }
+      } else if (num === null && num === false) {
+        alert("Вы завершили игру");
+      } else {
+        return gamePlay();
+      }
+    }
+  };
+};
+const startGame = gameStart();
+startGame();
