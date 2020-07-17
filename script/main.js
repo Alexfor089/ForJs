3ae898d942310381c1f547b8bea4638aa0dedac9
Hristofor Alekseev <hristofor2003@mail.ru>
2020-07-18 01:33:05 +0500

    Отправка форм работает,есть валидация ворм

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
   "use strict";
 
   //Timer
+
   function countTimer(deadline) {
     let timerHours = document.querySelector("#timer-hours"),
       timerMinutes = document.querySelector("#timer-minutes"),
@@ -207,15 +208,15 @@ window.addEventListener("DOMContentLoaded", function () {
         else if (target.matches(".dot"))
           dots.forEach(
             (item, index) =>
-              (currentSlide = target === item ? index : currentSlide)
+            (currentSlide = target === item ? index : currentSlide)
           );
         //если кол - во фото больше числа - то возращается к 1 (0)
         currentSlide =
-          currentSlide >= slides.length
-            ? 0
-            : currentSlide < 0
-            ? slides.length - 1
-            : currentSlide;
+          currentSlide >= slides.length ?
+          0 :
+          currentSlide < 0 ?
+          slides.length - 1 :
+          currentSlide;
 
         nextSlide(slides, currentSlide, "portfolio-item-active");
         nextSlide(dots, currentSlide, "dot-active");
@@ -275,7 +276,7 @@ window.addEventListener("DOMContentLoaded", function () {
       calcCount = document.querySelector(".calc-count"),
       calcDay = document.querySelector(".calc-day"),
       totalValue = document.getElementById("total");
-
+    // функция подсчета
     const countSum = () => {
       let total = 0,
         dayValue = 1,
@@ -295,9 +296,10 @@ window.addEventListener("DOMContentLoaded", function () {
       if (typeValue && squareValue) {
         total = price * typeValue * squareValue * countValue * dayValue;
       }
-      totalValue.textContent = total;
+      // totalValue
+      totalValue.textContent = Math.floor(total);
     };
-
+    //при смене - происходит подсчет(вызов функции countSum())
     calcBlock.addEventListener("change", (event) => {
       const target = event.target;
 
@@ -312,4 +314,104 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   };
   calculate(100);
+
+  //send-ajax=form
+
+
+  const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...',
+      loadMessage = `<div class="sk-rotating-plane"></div>`,
+      successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+    const form1 = document.getElementById('form1'),
+      form2 = document.getElementById('form2'),
+      form3 = document.getElementById('form3');
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = `font-size: 2rem;`;
+    //Валидация данных(в поля можно ввести  только цифры и знак “+”)
+    const valideDataPhone = (form) => {
+      if (form.querySelector('.form-phone')) {
+        form.querySelector('.form-phone').addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^[^+\d]*(\+|\d)|\D/g, '$1'));
+      }
+      //Кириллица 
+      if (form.querySelector('.form-name')) {
+        form.querySelector('.form-name').addEventListener('input', (e) => e.target.value = e.target.value.replace(/[^а-яА-Я ]/g, ''));
+      }
+      //И пробелы
+      if (form.querySelector('.form-message')) {
+        form.querySelector('.form-message').addEventListener('input', e => e.target.value = e.target.value.replace(/[^а-яА-Я ]/g, ''));
+      }
+    };
+    valideDataPhone(form1);
+    valideDataPhone(form2);
+    valideDataPhone(form3);
+    //Слушатели при отправке
+    form1.addEventListener('submit', e => {
+      e.preventDefault();
+      form1.appendChild(statusMessage);
+      statusMessage.innerHTML = loadMessage;
+      const formData = new FormData(form1);
+      let body = {};
+      formData.forEach((value, key) => body[key] = value);
+      postData(body)
+        .then(() => statusMessage.innerHTML = successMessage)
+        .catch(error => {
+          console.error(error);
+          statusMessage.innerHTML = errorMessage;
+        });
+
+      form1.querySelectorAll('input').forEach(item => item.value = '');
+    });
+    form2.addEventListener('submit', e => {
+      e.preventDefault();
+      form2.appendChild(statusMessage);
+      statusMessage.innerHTML = loadMessage;
+      const formData = new FormData(form2);
+      let body = {};
+      formData.forEach((value, key) => body[key] = value);
+      postData(body).then(() => statusMessage.innerHTML = successMessage)
+        .catch(error => {
+          console.error(error);
+          statusMessage.innerHTML = errorMessage;
+        });
+
+      form2.querySelectorAll('input').forEach(item => item.value = '');
+    });
+    form3.addEventListener('submit', e => {
+      e.preventDefault();
+      form3.appendChild(statusMessage);
+      statusMessage.style.color = '#fff';
+      statusMessage.innerHTML = loadMessage;
+
+      const formData = new FormData(form3);
+      let body = {};
+      formData.forEach((value, key) => body[key] = value);
+      postData(body)
+        .then(() => statusMessage.innerHTML = successMessage)
+        .catch(error => {
+          console.error(error);
+          statusMessage.innerHTML = errorMessage;
+        });
+
+      form3.querySelectorAll('input').forEach(item => item.value = '');
+    });
+
+    //Отправка данных на сервер
+
+    const postData = (body) => {
+      return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+          if (request.readyState !== 4) return;
+          if (request.status === 200) resolve();
+          else reject(request.status);
+        });
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(body));
+      });
+
+    }
+  };
+  sendForm();
 });
